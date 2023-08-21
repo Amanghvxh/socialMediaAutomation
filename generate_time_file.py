9f06c02af92e1bcbff9b5a9c06e1d4710e6c22d7
Amanghvxh <amanjandu3d@gmail.com>
2023-08-21 11:00:45 +1000

    finally

--- generate_time_file.py
+++ generate_time_file.py
@@ -4,14 +4,15 @@ import subprocess
 import os
 
 def generate_schedule():
-    start_time = datetime.strptime("08:00", "%H:%M")
+    # Randomize the start time between 8 AM and 9 AM
+    start_time = datetime.strptime("08:00", "%H:%M") + timedelta(minutes=random.randint(0, 59))
     end_time = datetime.strptime("21:00", "%H:%M")
     time_slots = [start_time]
 
-    for _ in range(6):  # 6 more times to generate, as we already have the start time
+    while len(time_slots) < 7:  # Ensure there are always 7 cron jobs
         # Add randomness to the time between cron jobs
         minutes_randomness = random.randint(0, 59)
-        next_time = time_slots[-1] + timedelta(hours=random.randint(2, 4), minutes=minutes_randomness)
+        next_time = time_slots[-1] + timedelta(hours=1) + timedelta(minutes=minutes_randomness)
         if next_time > end_time:
             break
         time_slots.append(next_time)
@@ -20,9 +21,9 @@ def generate_schedule():
 
 def schedule_cron_jobs(time_slots):
     # Get paths dynamically
-    current_dir = os.getcwd()
     python_path = subprocess.getoutput('which python3').strip()
-    script_path = os.path.join(current_dir, 'main.py').replace(" ", " ")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    script_path = os.path.join(script_dir, 'main.py')
 
     # Create new cron jobs for the generated times
     cron_jobs = []
@@ -30,6 +31,10 @@ def schedule_cron_jobs(time_slots):
         hour, minute = time_slot.strftime("%H:%M").split(":")
         cron_job = f'{minute} {hour} * * * "{python_path}" "{script_path}"'
         cron_jobs.append(cron_job)
+
+    # Schedule this script to run every day at 8 AM
+    self_schedule = f'0 8 * * * "{python_path}" "{os.path.abspath(__file__)}"'
+    cron_jobs.append(self_schedule)
 
     # Add the cron jobs
     with open('cronfile', 'w') as file:
@@ -39,7 +44,6 @@ def schedule_cron_jobs(time_slots):
     subprocess.run('rm cronfile', shell=True)
 
     print("Cron jobs scheduled.")
-
 
 if __name__ == "__main__":
     time_slots = generate_schedule()
--- update_json.py
+++ update_json.py
@@ -1,4 +1,11 @@
 import json
+import re
+
+def extract_quotes_from_content(content):
+    # Use regex to extract quotes and authors
+    pattern = r'\["(.*?)",\s*"(.*?)"\]'
+    matches = re.findall(pattern, content)
+    return matches
 
 def convert_to_object(quotes_list):
     return [{"quote": item[0], "author": item[1], "key": False} for item in quotes_list]
@@ -20,9 +27,10 @@ def update_pending_json(new_quotes):
         json.dump(data, file, indent=4)
 
 if __name__ == "__main__":
-    # Read the array from the file
+    # Read the content from the file
     with open('quotes.txt', 'r') as file:
-        quotes_array = eval(file.read())
+        content = file.read()
+        quotes_list = extract_quotes_from_content(content)
     
-    new_quotes = convert_to_object(quotes_array)
+    new_quotes = convert_to_object(quotes_list)
     update_pending_json(new_quotes)
